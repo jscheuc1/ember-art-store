0d70fa789dd2a0e2cf0fb128f5132a8d27a8c2fd
espa77 <ethansteiner@gmail.com>
2016-02-23 22:39:52 -0500

    valient attempt at uploading files

--- app/components/manage-piece.js
+++ app/components/manage-piece.js
@@ -1,24 +1,27 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  showAddArtistForm: false,
   actions: {
     attachArtist(){
       this.toggleProperty('showAddArtistForm');
     },
     submit(){
       let piece = this.get('piece');
+      // this.sendAction('onAddPiece', this.get('piece.asset'));
       let newArtist = {
         name: this.get('name'),
         location: this.get('location'),
         bio: this.get('bio'),
         asset: this.get('asset')
       };
-      if ('showAddArtistForm') {
-        this.sendAction('action', piece, newArtist);
+      if (this.get('showAddArtistForm')) {
+        this.sendAction('onAddPiece', piece, newArtist);
       }
       else {
-        let newArtist = null;
-        this.sendAction('action', piece, newArtist);
+        // let newArtist = null;
+        // this.sendAction('action', piece, newArtist);
+        this.sendAction('onAddPiece', piece);
       }
     }
   }
--- app/routes/pieces/new.js
+++ app/routes/pieces/new.js
@@ -1,11 +1,12 @@
 import Ember from 'ember';
-
 export default Ember.Route.extend({
   model(){
     return this.store.createRecord('piece');
   },
   actions: {
     addPiece(){
+      let file = document.getElementById('file-field').files[0];
+      this.set('model.asset', file);
       let piece = this.modelFor(this.routeName);
       piece.save().then((savedPiece) => {
         savedPiece.get('artists').invoke('save');
